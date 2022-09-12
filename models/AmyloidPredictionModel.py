802b8076d1d16a159561795ac2289a3b14cdc528
Anzu Sekikawa <aes186@pitt.edu>
2022-09-12 15:54:09 -0400

    update all

---
+++ models/AmyloidPredictionModel.py
@@ -0,0 +1,43 @@
+
+
+import torch
+from torch.utils.data import random_split, DataLoader
+import pandas as pd
+import torchio as tio
+import pytorch_lightning as pl
+import matplotlib.pyplot as plt
+import os
+import logging
+
+class Model(pl.LightningModule):
+    
+    def __init__(self, net, criterion, learning_rate, optimizer_class):
+        super().__init__()
+        self.lr = learning_rate
+        self.net = net
+        self.criterion = criterion
+        self.optimizer_class = optimizer_class
+
+    def configure_optimizers(self):
+        optimizer = self.optimizer_class(self.parameters(), lr=self.lr)
+        return optimizer
+
+    def prepare_batch(self, batch):
+        return batch['image'][tio.DATA], batch['label'][tio.DATA]
+
+    def infer_batch(self, batch):
+        x, y = self.prepare_batch(batch)
+        y_hat = self.net(x)
+        return y_hat, y
+
+    def training_step(self, batch, batch_idx):
+        y_hat, y = self.infer_batch(batch)
+        loss = self.criterion(y_hat, y)
+        self.log('train_loss', loss, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        y_hat, y = self.infer_batch(batch)
+        loss = self.criterion(y_hat, y)
+        self.log('val_loss', loss)
+        return loss
---
+++ models/AmyloidPrediction_Train.py
@@ -0,0 +1,89 @@
+
+import torch
+from torch.utils.data import random_split, DataLoader
+import pandas as pd
+import torchio as tio
+import pytorch_lightning as pl
+import matplotlib.pyplot as plt
+import os
+import logging
+from tqdm import tqdm 
+
+import monai #?? 
+
+import AmyloidPredictionDataModule 
+import AmyloidPredictionModel as Model 
+
+import time 
+from datetime import datetime 
+
+# from unet import Unet 
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+
+# instantiate dataModule 
+data = AmyloidPredictionDataModule(
+    batch_size = 16, # TODO: check this 
+    train_val_ratio = 0.8
+)
+
+# prepare data 
+data.prepare_data() 
+
+# setup data 
+data.setup() 
+
+print('Training: ', len(data.train_set))
+print('Validation: ', len(data.val_set))
+print('Test:      ', len(data.test_set))
+
+# the model 
+unet = monai.networks.nets.UNet(
+    dimensions=3,
+    in_channels=1,
+    out_channels=3,
+    channels=(8, 16, 32, 64),
+    strides=(2, 2, 2),
+)
+
+# instantiating the model 
+model = Model( 
+    net = unet,
+    criterion = torch.nn.BCELoss(),
+    learning_rate = 1e-2,
+    optimizer_class = torch.optim.AdamW #TODO: check this
+)
+
+early_stopping = pl.callbacks.early_stopping.EarlyStopping(
+    monitor='val_loss',
+)
+
+# Trainer 
+trainer = pl.Trainer(
+    gpus=1 if torch.cuda.is_available() else 0,
+    precision=16,
+    callbacks=[early_stopping],
+)
+
+trainer.logger._default_hp_metric = False
+
+start = datetime.now()
+
+print('Training started at', start)
+
+# trainer.fit - loads in our model and data module 
+trainer.fit(model=model, datamodule=data)
+
+print('Training duration:', datetime.now() - start)
+
+
+
+
+
+
+
+
+
+
+
